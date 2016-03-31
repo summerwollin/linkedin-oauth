b72852b551f59af84232a1e38657694b3b49c290
Summer Wollin <summer.wollin@gmail.com>
2016-03-31 14:50:41 -0600

    display username on pages

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cookieSession = require('cookie-session');
 
 var LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
 var passport = require('passport');
@@ -26,9 +27,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(cookieSession({
+  secret: process.env.LINKEDIN_CLIENT_SECRET
+}));
 
 app.get('/auth/linkedin',
-passport.authenticate('linkedin', {state: true}),
+passport.authenticate('linkedin'),
 function(req, res){
   // The request will be redirected to LinkedIn for authentication, so this
   // function will not be called.
@@ -44,15 +50,9 @@ passport.use(new LinkedInStrategy({
   clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
   callbackURL: process.env.HOST + "/auth/linkedin/callback",
   scope: ['r_emailaddress', 'r_basicprofile'],
+  state: true
 }, function(accessToken, refreshToken, profile, done) {
-  // asynchronous verification, for effect...
-  process.nextTick(function () {
-    // To keep the example simple, the user's LinkedIn profile is returned to
-    // represent the logged-in user. In a typical application, you would want
-    // to associate the LinkedIn account with a user record in your database,
-    // and return that user instead.
-    return done(null, profile);
-  });
+  done(null, {id: profile.id, displayName: profile.displayName})
 }));
 
 
@@ -63,6 +63,11 @@ passport.serializeUser(function(user, done) {
 passport.deserializeUser(function(user, done) {
   done(null, user)
 });
+
+app.use(function (req, res, next) {
+  res.locals.user = req.session.passport.user
+  next()
+})
 
 app.use('/', routes);
 app.use('/users', users);
